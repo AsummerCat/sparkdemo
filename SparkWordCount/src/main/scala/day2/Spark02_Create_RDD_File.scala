01a704341002e65f13230405bc308c1e81e0099a
chenxiaocheng <583188551@qq.com>
2022-08-23 17:45:29 +0800

    textFile

--- SparkWordCount/src/main/scala/day2/Spark02_Create_RDD_File.scala
+++ SparkWordCount/src/main/scala/day2/Spark02_Create_RDD_File.scala
@@ -1,10 +1,11 @@
 package day2
 
 import org.apache.spark.{SparkConf, SparkContext}
+
 /**
- * RDD从内存中获取数据
+ * RDD从文件中获取数据
  */
-object Spark02_Create_RDD_Memory {
+object Spark02_Create_RDD_File {
   def main(args: Array[String]): Unit = {
 
     //创建scala和Spark框架的链接
@@ -12,9 +13,7 @@ object Spark02_Create_RDD_Memory {
     val sc = new SparkContext(sparkConf)
     //执行业务逻辑
     //1.创建RDD 并行RDD(业务列表,调用CPU核数分片处理)
-    val rdd1 = sc.parallelize(List(1,2,3,4,5), 2)
-//    等价上面那个 makeRDD方法底层就是调用了parallelize方法
-//    val rdd1 = sc.makeRDD(List(1,2,3,4,5), 2)
+    val rdd1 = sc.textFile("SparkWordCount\\src\\data\\2.txt", 2).flatMap(_.split(" "));
 
     //计算逻辑
     rdd1.collect().foreach(println)
