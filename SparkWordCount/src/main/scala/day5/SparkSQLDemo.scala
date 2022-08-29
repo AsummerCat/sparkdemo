89aae3c5a7b053bb4e208d919a523c7a9cbb68dc
583188551 <583188551@qq.com>
2022-08-30 04:46:17 +0800

    修复版本不一致问题

--- SparkWordCount/src/main/scala/day5/SparkSQLDemo.scala
+++ SparkWordCount/src/main/scala/day5/SparkSQLDemo.scala
@@ -1,10 +1,7 @@
 package day5
 
-import org.apache.spark
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.{SparkConf, SparkContext}
-
-import scala.collection.immutable
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 /**
  * 使用sparkSql 基础语法
@@ -14,20 +11,16 @@ import scala.collection.immutable
  */
 object SparkSQLDemo {
   def main(args: Array[String]): Unit = {
+    //1.读取配置
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkSql")
-    //1.读取资源
-
     //2.创建SQLcontext对象
-    val sqlc = SparkSession.builder().config(sparkConf).getOrCreate()
+    val sqlc: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
 
     //3.创建sql查询操作
     val df = sqlc.read.json("SparkWordCount\\src\\data\\user.json")
     //或者
-//    val frame: DataFrame = sqlc.sql("select * from user")
-    val list = immutable.List(List(df))
+    //    val frame: DataFrame = sqlc.sql("select * from user")
+    df.show()
 
-    //3.查询语句
-    sqlc.sql("select * from user")
-    list.foreach(print)
   }
 }
